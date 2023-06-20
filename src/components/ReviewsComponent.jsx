f53917240b2cb0d766736757c3a1a159269d17dc
Erich Richter <erich.richter@gmail.com>
2023-06-20 11:10:01 -0700

    format review component

--- src/components/ReviewsComponent.jsx
+++ src/components/ReviewsComponent.jsx
@@ -3,19 +3,34 @@ import { styled } from "styled-components";
 
 const ReviewContainer = styled.div`
   display: flex;
+
   gap: 10px;
+`;
+const Img = styled.img`
+  display: flex;
+  height: 50px;
+  width: 50px;
+  border-radius: 50px;
+  border-color: white;
+  border-width: 2px;
+  border-style: solid;
+  margin-top: 10px;
+  margin-bottom: 10px;
 `;
 
 const Review = styled.div`
+  display: flex;
+  flex-direction: column;
+
   font-weight: 10;
   letter-spacing: 1px;
   line-height: 1.5;
   background-color: #582727;
   color: white;
 
-  width: 100%;
+  width: 50%;
 
-  padding: 10px;
+  padding: 20px;
   border: none;
 
   border-radius: 5px;
@@ -23,10 +38,23 @@ const Review = styled.div`
   border-color: white;
   border-style: solid;
   border-radius: 5px;
-  /* &:before {
-    content: " 🦄Reviews:";
-    position: absolute;
-  } */
+`;
+
+const ReviewPic = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 20px;
+`;
+const Button = styled.button`
+  margin-top: 10px;
+  background-color: #434343;
+  color: white;
+  font-weight: 500px;
+  width: 200px;
+  padding: 10px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
 `;
 
 const ReviewsComponent = () => {
@@ -50,11 +78,21 @@ const ReviewsComponent = () => {
         <p>No reviews found.</p>
       ) : (
         reviews.map((review) => (
-          <Review>
-            <div key={review.id}>
+          <Review key={review.id}>
+            <ReviewPic>
+              {review.user.image_url ? (
+                <Img src={review.user.image_url} />
+              ) : (
+                <Img src="./img/blank-profile-picture.png" />
+              )}
               <h3>{review.user.name}</h3>
-              {review.text}
-            </div>
+            </ReviewPic>
+            {review.text}
+
+            <a href={`${review.url}`}>
+              <Button>Go to full review</Button>
+            </a>
+            {/* </div> */}
           </Review>
         ))
       )}
